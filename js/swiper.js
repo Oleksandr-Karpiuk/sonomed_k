0f4b71e34a71c9a222362410aec2a815c98f8c0b
Oleksandr Karpiuk <alex.karpuyk@gmail.com>
2023-05-26 12:49:38 +0300

    Add swiper to services

---
+++ js/swiper.js
@@ -0,0 +1,22 @@
+var swiper = new Swiper(".mySwiperServices", {
+  effect: "coverflow",
+  loop: true,
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
