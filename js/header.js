90e8e0eb07dcd1140545397ec7ebb0f0d7d73d39
Oleksandr Karpiuk <alex.karpuyk@gmail.com>
2023-08-08 15:45:59 +0300

    Adds animation at header

--- js/header.js
+++ js/header.js
@@ -19,3 +19,27 @@ window.addEventListener("scroll", () => {
 
   lastScroll = scrollPosition();
 });
+
+// Приховування хедера
+
+// let lastScroll = 0;
+// const defaultOffset = 400;
+// const header = document.querySelector(".header");
+
+// const scrollPosition = () =>
+//   window.pageYOffset || document.documentElement.scrollTop;
+// const containHide = () => header.classList.contains("hide");
+
+// window.addEventListener("scroll", () => {
+//   if (
+//     scrollPosition() > lastScroll &&
+//     !containHide() &&
+//     scrollPosition() > defaultOffset
+//   ) {
+//     header.classList.add("hide");
+//   } else if (scrollPosition() < lastScroll && containHide()) {
+//     header.classList.remove("hide");
+//   }
+
+//   lastScroll = scrollPosition();
+// });
